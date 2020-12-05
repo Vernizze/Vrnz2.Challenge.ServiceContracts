f346b81c9b99291053b8f5d88a1606ba21914a64
Vernizze <cfvernizze@gmail.com>
2020-12-05 18:10:43 -0300

    Inclusao do Model de criacao de Clientes

---
+++ Vrnz2.Challenge.ServiceContracts/Responses/Management/Clients/PingController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Vrnz2.Challenge.ServiceContracts.Responses.Management.Clients
+{
+    public class PingController
+    {
+        public class Ping
+        {
+            public string ServiceDateTime { get; } = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:mm");
+            public string ServiceName { get; } = AppDomain.CurrentDomain.FriendlyName;
+        }
+    }
+}
